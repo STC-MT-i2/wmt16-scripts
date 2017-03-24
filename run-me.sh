ae829a4ab86ff6da49c14d1fcd09e4e4d8a769a4
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2017-03-24 17:54:56 +0100

    change paths for amunmt

--- run-me.sh
+++ run-me.sh
@@ -22,17 +22,22 @@ then
     ./scripts/preprocess.sh
 fi
 
+if [ ! -e "model/model.iter90000.npz" ]
+then
+
 ../../build/marian \
  --model model/model.npz \
  --devices 0 \
  --train-sets data/corpus.bpe.ro data/corpus.bpe.en \
+ --vocabs model/vocab.ro.yml model/vocab.en.yml \
  --dim-vocabs 50000 50000 \
  --mini-batch 80 \
  --layer-normalization \
- --early-stopping 5 --after-batches 1000 \
- --valid-freq 1000 --save-freq 30000 --disp-freq 100 \
+ --after-batches 90000 \
+ --valid-freq 10000 --save-freq 30000 --disp-freq 1000 \
  --valid-sets data/newsdev2016.bpe.ro data/newsdev2016.bpe.en \
  --valid-metrics cross-entropy valid-script \
  --valid-script-path ./scripts/validate.sh \
  --log model/train.log --valid-log model/valid.log
 
+fi
